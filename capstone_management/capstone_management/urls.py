19692aa5e6d87f61a82c292dcc929f184326ac59
Kosala <dinuth2013@gmail.com>
2019-10-04 13:09:05 +0530

    Login url update

--- capstone_management/capstone_management/urls.py
+++ capstone_management/capstone_management/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from CPPMS import views
 
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('CPPMS/', include('CPPMS.urls')),
     path('admin/', admin.site.urls),
 ]
